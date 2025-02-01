a78c38038c31de6a9daf5bbba2829bfc9f01f431
Davidon4 <juggernaut.dev1@gmail.com>
2025-02-01 22:23:27 +0100

    Update SDK code

--- Tests/ReddimonAttributionTests/AttributionManagerTests.swift
+++ Tests/ReddimonAttributionTests/AttributionManagerTests.swift
@@ -6,7 +6,22 @@ final class AttributionManagerTests: XCTestCase {
         XCTAssertNotNil(AttributionManager.shared)
     }
     
+    func testHandleAttributionLink() {
+        let url = URL(string: "yourapp://install?creator_id=123")!
+        AttributionManager.shared.handleAttributionLink(url)
+    }
+    
+    func testTrackConversion() {
+        AttributionManager.shared.trackConversion(
+            type: "subscription",
+            value: 99.99,
+            currency: "USD"
+        )
+    }
+    
     static var allTests = [
         ("testInitialization", testInitialization),
+        ("testHandleAttributionLink", testHandleAttributionLink),
+        ("testTrackConversion", testTrackConversion),
     ]
 } 
